8e4b23d3ed2e5086840948cb8cadbc24a22f91fb
Daniel Larios <daniel.larios.torres@nui.care>
2022-09-18 21:12:58 +0200

    correcion

--- control/monitor.py
+++ control/monitor.py
@@ -14,7 +14,7 @@ def analyze_data_custom():
     
     print("Calculando alertas de humedad...")
     
-    data = Data.objects.filter( base_time__gte=datetime.now() - timedelta(hours=1))
+    data = Data.objects.filter( base_time__gte=datetime.now() - timedelta(minutes=1))
     aggregation  = data.annotate(check_value=Max('max_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
